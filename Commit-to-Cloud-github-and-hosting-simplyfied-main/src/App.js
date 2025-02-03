7208c6a4a85af06efbaf9ae15546a7a1be0779c5
arnavm2505 <155662436+arnavm2505@users.noreply.github.com>
2025-02-03 17:15:47 +0530

    Update App.js

--- Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
+++ Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong>Name : Arnav  M</p> 
-          <p><strong>USN:</strong>USN : 1RVU23CSE077</p>
+          <p><strong>Name:</strong> Arnav M</p> 
+          <p><strong>USN:</strong> 1RVU23CSE077</p>
         </div>
       </header>
     </div>
